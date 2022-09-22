04261624fe99b74e6795a39044b0532f0eefbf20
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-21 22:29:05 -0300

    links a rutas añadidos a la navegacion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import { Menu } from './Menu'
+import { Link } from 'react-router-dom'
+// import { Menu } from './Menu'
 export const Navbar = () => {
   return (
     <div>
-      <Menu />
+      {/* <Menu /> */}
+      <div>
+        <Link to={'/'}>Inicio</Link>
+      </div>
+      <div>
+        <Link to={'/albums'}>Biblioteca</Link>
+      </div>
     </div>
   )
 }
