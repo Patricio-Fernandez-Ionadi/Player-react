2596c6cb551fa53cb8872cb413a61039fdd3ca84
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-21 22:26:14 -0300

    ref del player context provider

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
-import { PlayerContextProvider } from './context'
 import { App } from './epApp'
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <PlayerContextProvider>
-        <App />
-      </PlayerContextProvider>
+      <App />
     </BrowserRouter>
   )
 }
