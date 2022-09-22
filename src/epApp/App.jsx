456c60433d4f45236decccc6fe3f2ed09b883ffb
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-21 22:29:52 -0300

    estilos de layout establecidos styled-components

--- src/epApp/App.jsx
+++ src/epApp/App.jsx
@@ -1,11 +1,49 @@
+import { PlayerContextProvider } from 'context'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import styled, { css } from 'styled-components'
 import { Navbar, Player } from '../components'
+
+const MainContainer = styled.div`
+  height 100vh;
+  max-height 100vh;
+  width 100vw;
+  display flex;
+`
+
+const Content = styled.div`
+  flex-grow 1;
+  background-color yellow;
+  padding 10px;
+`
+
+const StyledNavigation = styled.div`
+  width 50px;
+  height 100%;
+`
 
 export const App = () => {
   return (
     <>
-      <Navbar />
-      <Player />
+      <MainContainer>
+        <StyledNavigation>
+          <Navbar />
+        </StyledNavigation>
+
+        <Content>
+          <PlayerContextProvider>
+            <Player />
+          </PlayerContextProvider>
+          <Routes>
+            {/* PRIVATE ROUTES */}
+            <Route path='/' element={<h1>Inicio</h1>} />
+            <Route path='/albums' element={<h1>Biblioteca</h1>} />
+            {/* PUBLIC ROUTES */}
+            <Route path='/login' element={<h1>Login</h1>} />
+            <Route path='/register' element={<h1>Register</h1>} />
+          </Routes>
+        </Content>
+      </MainContainer>
     </>
   )
 }
