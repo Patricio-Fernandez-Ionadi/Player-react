39fd44a15536349bb18a86aff3ea5aae8e2a40e1
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-21 22:28:43 -0300

    reordenamiento de componentes

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -5,6 +5,7 @@ import {
   ControlPrev,
   ControlVolume,
   ControlNext,
+  ControlRepeat,
 } from './Controls'
 
 export const Player = () => {
@@ -13,22 +14,27 @@ export const Player = () => {
   return (
     <div>
       <div>
+        <ControlRepeat />
         <ControlVolume />
+        {/* this may be a component apart later */}
+        <div>
+          <p>
+            {currentSong.song} - <span>{currentSong.duration}</span>
+          </p>
+          {/* for now is a read only element but could have an onChange function that set the new point of playing of the current song */}
+          <input
+            type='range'
+            value={percentSong}
+            readOnly
+            alt='percent playing'
+          />
+        </div>
       </div>
       <div>
         <ControlMute />
         <ControlPrev />
         <ControlPlayPause />
         <ControlNext />
-      </div>
-
-      {/* this may be a component apart later */}
-      <div>
-        <p>
-          {currentSong.song} - <span>{currentSong.duration}</span>
-        </p>
-        {/* for now is a read only element but could have an onChange function that set the new point of playing of the current song */}
-        <input type='range' value={percentSong} readOnly />
       </div>
     </div>
   )
