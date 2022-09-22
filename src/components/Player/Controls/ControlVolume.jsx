a9bd332829985f890d38471886e2e87db3752b78
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-21 22:24:57 -0300

    accesibilidad y set de volumen en primer render

--- src/components/Player/Controls/ControlVolume.jsx
+++ src/components/Player/Controls/ControlVolume.jsx
@@ -2,16 +2,25 @@ import { useEffect, useRef } from 'react'
 import { usePlayerContext } from 'context'
 
 export const ControlVolume = () => {
+  // Input element
+  const volumeControl = useRef(HTMLInputElement)
+  // context
   const { volume } = usePlayerContext()
-  const volumeControl = useRef()
 
+  // sets the default value for the volume to 100% at the moment when app starts
   useEffect(() => {
-    volumeControl.current.value = '100'
-  }, [volumeControl.current])
+    volumeControl ? (volumeControl.current.value = '100') : null
+  }, [])
 
+  // sets the volume % in the context when user changes its value
   const hanldeVolumeChange = (e) => volume.setVolume(e.target.value)
 
   return (
-    <input type='range' ref={volumeControl} onChange={hanldeVolumeChange} />
+    <input
+      type='range'
+      ref={volumeControl}
+      onChange={hanldeVolumeChange}
+      alt='volume input'
+    />
   )
 }
