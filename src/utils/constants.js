8b0d4d0585ea1820bab607228a0686541bcc6bc2
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-22 00:31:43 -0300

    set colors para hacer theming

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,16 @@
 export const baseApiUri = 'http://localhost:3001/api'
+
+export const colors = {
+  primary_light: '#00FFF5',
+  primary: '#00ADB5',
+  primary_dark: '#0D7377',
+  dark: '#323232',
+  darken: '#323232',
+  black: '#100F0F',
+  white: '#EEEEEE',
+  succes: '#C4FB6D',
+  error: '#FF2E63',
+  blue: '#2F89FC',
+  grey: '#474744',
+  grey_light: '#DADADA',
+}
