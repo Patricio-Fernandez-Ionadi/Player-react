e47b681c99eb7ddaedd3a0c65b0517759e3acdb9
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-24 19:06:28 -0300

    estilos propios del player usados dentro de su carpeta

---
+++ src/Player/style/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import { colors } from 'utils/constants'
+
+export const StyledPlayerButton = styled.button`
+  background: ${colors.background};
+  width: 60px;
+  padding: 5px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 10px;
+  color: ${colors.white};
+  transition: background 0.2s;
+  position: relative;
+  box-shadow: 1px 1px 3px 0 ${colors.dark};
+  &:hover {
+    background: ${colors.primary_dark};
+  }
+`
