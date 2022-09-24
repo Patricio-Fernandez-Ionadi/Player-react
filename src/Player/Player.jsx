62937f9dfc6d05f37bda9be811b4dbd4a56d4e1c
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-24 00:00:02 -0300

    estilizacion y organizacion del player principal

--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -1,22 +1,39 @@
-import styled from 'styled-components'
-import { Accesibility } from './Accesibility/Accesibility'
-import { ControlPlayPause, ControlPrev, ControlNext } from './Controls'
+import { usePlayerContext } from 'context'
+import {
+  ControlPlayPause,
+  ControlPrev,
+  ControlNext,
+  ControlRepeat,
+  ControlVolume,
+  ControlMute,
+} from './Controls'
+import { SongInfo, Statusbar } from './Statusbar'
 
-const PlayerContainer = styled.div`
-  width 100%;
-  display flex;
-  flex-direction column;
-  justify-content center;
-  align-items center;
-`
+import './style/index.css'
 
 export const Player = () => {
   return (
-    <PlayerContainer>
-      <Accesibility />
-      <ControlPrev />
-      <ControlPlayPause />
-      <ControlNext />
-    </PlayerContainer>
+    <div className='player'>
+      <div className='volume-control-container'>
+        <div className='volume-control-box'>
+          <ControlMute />
+          <ControlVolume />
+        </div>
+      </div>
+
+      <div className='comands-control-container'>
+        <div className='status-box'>
+          <SongInfo />
+          <Statusbar />
+        </div>
+
+        <div className='controls-box'>
+          <ControlRepeat />
+          <ControlPrev />
+          <ControlPlayPause />
+          <ControlNext />
+        </div>
+      </div>
+    </div>
   )
 }
