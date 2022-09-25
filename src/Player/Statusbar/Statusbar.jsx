b32903519eb6b442dda4221ceed04d3003e655e8
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-25 16:59:35 -0300

    da uso a usePercentPlayed hook y se deshace de logica

--- src/Player/Statusbar/Statusbar.jsx
+++ src/Player/Statusbar/Statusbar.jsx
@@ -1,12 +1,15 @@
-import { usePlayerContext } from 'context'
+import { useSelector } from 'react-redux'
+import { usePercentPlayed } from 'hooks/usePercentPlayed'
 
 export const Statusbar = () => {
-  const { percentSong } = usePlayerContext()
+  const { currentSong } = useSelector(({ player }) => player)
+  usePercentPlayed() // updates the current percent played of the song
+
   return (
     //  at the moment it is a read only element but could have an onChange function that set the new point of playing of the current song
     <input
       type='range'
-      value={percentSong}
+      value={currentSong.percent_played || 0}
       readOnly
       alt='percent playing'
       aria-label='Transcurred time of the song'
