40ebfefc67d1508bb06fb408f6890b54baca3a9c
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-25 16:58:48 -0300

    presentacion de rutas

--- src/epApp/App.jsx
+++ src/epApp/App.jsx
@@ -1,15 +1,15 @@
+import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 // context
 import { PlayerContextProvider } from 'context'
+import { initSongs } from 'store'
 // components
 import { Navbar } from 'components'
 import { Player } from 'Player'
 // styles
 import './style/index.css'
-import { useEffect } from 'react'
-import { initSongs } from 'store'
 
 export const App = () => {
   const dispatch = useDispatch()
@@ -31,8 +31,9 @@ export const App = () => {
           <Routes>
             {/* PRIVATE ROUTES */}
             <Route path='/' element={<h1>Inicio</h1>} />
-            <Route path='/albums' element={<h1>Biblioteca</h1>} />
+            <Route path='/playlist' element={<h1>Playlist</h1>} />
             {/* PUBLIC ROUTES */}
+            <Route path='/lista' element={<h1>Lista</h1>} />
             <Route path='/login' element={<h1>Login</h1>} />
             <Route path='/register' element={<h1>Register</h1>} />
           </Routes>
