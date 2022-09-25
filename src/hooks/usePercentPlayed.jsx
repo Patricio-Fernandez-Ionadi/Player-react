74e055adbf5f6705fe5d903ea5f0d9cbaa3c1fbd
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-25 16:56:03 -0300

    custom hook to updates the current time of playing

---
+++ src/hooks/usePercentPlayed.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPercentPlayed } from 'store'
+
+// updates the percent played of the song with its current time of playing and the total time of the song
+
+export const usePercentPlayed = () => {
+  const dispatch = useDispatch()
+  const { currentSong, isPlaying, html_audio } = useSelector(
+    ({ player }) => player
+  )
+
+  const dispatchUpdatePercentPlayed = () => () => {
+    if (html_audio && currentSong && isPlaying) {
+      console.log('dispatching')
+      dispatch(setPercentPlayed(html_audio, currentSong))
+    }
+  }
+
+  useEffect(() => {
+    let oneSecInterval
+    if (isPlaying) {
+      oneSecInterval = setInterval(dispatchUpdatePercentPlayed(), 1000)
+    }
+    return () => clearInterval(oneSecInterval)
+  }, [isPlaying])
+}
