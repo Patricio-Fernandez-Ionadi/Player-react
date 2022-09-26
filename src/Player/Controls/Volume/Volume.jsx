7c3edbb943b48b00576327b5aa38d52a05e7c588
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-26 20:50:49 -0300

    se elimina funcionalidad de abrir y cerrar el panel de volumen

--- src/Player/Controls/Volume/Volume.jsx
+++ src/Player/Controls/Volume/Volume.jsx
@@ -1,5 +1,3 @@
-import { useRef, useState } from 'react'
-
 // context
 import { useSelector } from 'react-redux'
 // theme
@@ -13,44 +11,18 @@ import { ControlMute } from '../Mute'
 
 export const Volume = () => {
   const { isMuted } = useSelector(({ player }) => player)
-  const volumeButtonRef = useRef()
-
-  const [volumePanelOpen, setVolumePanelOpen] = useState(false)
-  const [anchorEl, setAnchorEl] = useState(null)
-
-  let outsideOfButton = (e) =>
-    volumeButtonRef.current && !volumeButtonRef.current.contains(e.target)
-
-  // if users click on button sets the anchorEl, otherwise its goin to be null
-  window.onclick = (e) => {
-    if (outsideOfButton(e)) {
-      setAnchorEl(null)
-      setVolumePanelOpen(false)
-    } else setAnchorEl(volumeButtonRef.current)
-  }
-
-  // if anchorEl exist and the panel is already open: close it, else if anchorEl is null or panel is closed: open it
-  const handleVolumeControl = () => {
-    if (anchorEl && volumePanelOpen) {
-      setVolumePanelOpen(false)
-    } else {
-      setVolumePanelOpen(true)
-    }
-  }
 
   return (
     <div className='volume-control-container'>
-      {(volumePanelOpen || isMuted) && <ControlMute />}
+      <ControlMute />
       <button
-        onClick={handleVolumeControl}
         aria-label='volume control button'
         type='button'
-        ref={volumeButtonRef}
         className='btn volume-button'
       >
         {isMuted ? <MuteIcon /> : <VolumeIcon />}
       </button>
-      {volumePanelOpen && <ControlVolume />}
+      <ControlVolume />
     </div>
   )
 }
