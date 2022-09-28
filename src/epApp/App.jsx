76248ea500de41f3746d7a03c14aa10c59cd5b5d
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-27 21:53:49 -0300

    app por ahora maneja el evento de click en nuestra app para solventar la apertura y cierre de menu

--- src/epApp/App.jsx
+++ src/epApp/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
@@ -9,36 +9,44 @@ import { initSongs } from 'store'
 import { Navbar } from 'components'
 import { Player } from 'Player'
 // styles
-import './style/index.css'
+import '../theme/index.css'
 
 export const App = () => {
+  const [isOpen, setIsOpen] = useState()
+
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(initSongs())
   }, [dispatch])
 
-  return (
-    <>
-      <div className='main-container-app'>
-        <nav className='navigation-container-app'>
-          <Navbar />
-        </nav>
+  const handleAppClick = (e) => {
+    if (window.innerWidth > 739) {
+      if (!isOpen && e.clientX < 40 && e.clientY < 40) {
+        setIsOpen(true)
+      } else if (isOpen) {
+        setIsOpen(false)
+      }
+    }
+  }
 
-        <div className='content-container-app'>
-          <PlayerContextProvider>
-            <Player />
-          </PlayerContextProvider>
-          <Routes>
-            {/* PRIVATE ROUTES */}
-            <Route path='/' element={<h1>Inicio</h1>} />
-            <Route path='/playlist' element={<h1>Playlist</h1>} />
-            {/* PUBLIC ROUTES */}
-            <Route path='/lista' element={<h1>Lista</h1>} />
-            <Route path='/login' element={<h1>Login</h1>} />
-            <Route path='/register' element={<h1>Register</h1>} />
-          </Routes>
-        </div>
+  return (
+    <div className='main-container-app' onClick={handleAppClick}>
+      <Navbar isopen={isOpen} />
+
+      <div className='content-container-app'>
+        <PlayerContextProvider>
+          <Player />
+        </PlayerContextProvider>
+        <Routes>
+          {/* PRIVATE ROUTES */}
+          <Route path='/' element={<h1>Inicio</h1>} />
+          <Route path='/playlist' element={<h1>Playlist</h1>} />
+          {/* PUBLIC ROUTES */}
+          <Route path='/lista' element={<h1>Lista</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/register' element={<h1>Register</h1>} />
+        </Routes>
       </div>
-    </>
+    </div>
   )
 }
