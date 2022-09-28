1f7f9689a600e036aa0a07570b991bc7f8809a79
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-27 21:54:21 -0300

    navbar recibe comportamiento

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,51 @@
 import { Link } from 'react-router-dom'
-import './style/index.css'
+import { Menu } from './Menu'
 
-export const Navbar = () => {
+export const Navbar = ({ isopen }) => {
   return (
-    <ul className='navbar-list-container'>
-      <li className='navbar-list-element'>
-        <Link className='navbar-list-link' to={'/'}>
-          Inicio
-        </Link>
-      </li>
-      <li className='navbar-list-element'>
-        <Link className='navbar-list-link' to={'/lista'}>
-          Lista
-        </Link>
-      </li>
-      <li className='navbar-list-element'>
-        <Link className='navbar-list-link' to={'/playlist'}>
-          Playlist
-        </Link>
-      </li>
-      <li className='navbar-list-element'>
-        <Link className='navbar-list-link' to={'/login'}>
-          Login
-        </Link>
-      </li>
-    </ul>
+    <nav
+      className={
+        isopen
+          ? `navigation-container-app menu-open`
+          : 'navigation-container-app'
+      }
+    >
+      <Menu state={isopen} />
+
+      <ul className='navbar-list-container'>
+        <li className='navbar-list-element'>
+          <Link className='navbar-list-link' to={'/'}>
+            Inicio
+          </Link>
+          <div className='non-mobile'>
+            <button type='button'>iconito</button>
+          </div>
+        </li>
+        <li className='navbar-list-element'>
+          <Link className='navbar-list-link' to={'/lista'}>
+            Lista
+          </Link>
+          <div className='non-mobile'>
+            <button type='button'>iconito</button>
+          </div>
+        </li>
+        <li className='navbar-list-element'>
+          <Link className='navbar-list-link' to={'/playlist'}>
+            Playlist
+          </Link>
+          <div className='non-mobile'>
+            <button type='button'>iconito</button>
+          </div>
+        </li>
+        <li className='navbar-list-element'>
+          <Link className='navbar-list-link' to={'/login'}>
+            Login
+          </Link>
+          <div className='non-mobile'>
+            <button type='button'>iconito</button>
+          </div>
+        </li>
+      </ul>
+    </nav>
   )
 }
