8e17539f9ec8daa117438b96453b6185d958f7de
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-30 00:02:40 -0300

    add consume el contexto del menu para estilos

--- src/components/Navbar/List/List.jsx
+++ src/components/Navbar/List/List.jsx
@@ -1,12 +1,17 @@
+import { useMenuMobileContext } from 'context'
 import { ListElement } from '.'
 
 export const List = () => {
+  const { isOpen } = useMenuMobileContext()
+
+  const openMenuMobileClass = isOpen ? 'menu-open' : ''
+
   return (
-    <ul className='navbar-list-container'>
-      <ListElement route='/' label='Inicio' />
+    <ul className={`navbar-list-container ${openMenuMobileClass}`}>
+      {/* <ListElement route='/' label='Inicio' /> */}
       <ListElement route='/lista' label='Lista' />
       <ListElement route='/playlist' label='Playlist' />
-      <ListElement route='/login' label='Login' />
+      {/* <ListElement route='/login' label='Login' /> */}
     </ul>
   )
 }
