bc1175cf8e070313288594c84143d3e8e54d8760
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-30 21:29:55 -0300

    ref ya no consume el contexto

--- src/Player/Controls/Prev/ControlPrev.jsx
+++ src/Player/Controls/Prev/ControlPrev.jsx
@@ -1,15 +1,22 @@
-// external
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
 
-// own
-// context
-import { usePlayerContext } from 'context'
-
 export const ControlPrev = () => {
-  const { prev } = usePlayerContext()
   return (
-    <button className='btn' onClick={prev} aria-label='prev button'>
+    <button className='btn' onClick={() => {}} aria-label='prev button'>
       <SkipPreviousIcon />
     </button>
   )
 }
+
+// for now just reset the corrent song and after a sec play it again
+// const prev = (e) => {
+//   // console.log(e.detail) // veces seguidas que se recibe el evento de click
+//   if (track.current.currentTime > 0) {
+//     track.current.load()
+//     if (isPlaying) {
+//       setTimeout(() => {
+//         track.current.play()
+//       }, 1000)
+//     }
+//   }
+// }
