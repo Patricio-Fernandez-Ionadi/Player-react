9613ed02582a1a94f88937a5517dbde303336ee3
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-13 22:08:47 -0300

    ref vuelve a usar contexto

--- src/Player/Controls/Next/ControlNext.jsx
+++ src/Player/Controls/Next/ControlNext.jsx
@@ -2,50 +2,55 @@ import { useState } from 'react'
 // external
 import SkipNextIcon from '@mui/icons-material/SkipNext'
 // store
-import { useDispatch, useSelector } from 'react-redux'
-import { nextSong, startAgain } from 'store'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { nextSong, startAgain } from 'store'
 // utils
 import { continuePlaying } from 'utils/helpers'
+import { usePlayerContext } from 'Player/Context'
 
 export const ControlNext = () => {
   // store
-  const dispatch = useDispatch()
-  const { html_audio, currentSong, songs, isPlaying, repeatAll } = useSelector(
-    ({ player }) => player
-  )
+  // const dispatch = useDispatch()
+  // const { html_audio, currentSong, songs, isPlaying, repeatAll } = useSelector(
+  //   ({ player }) => player
+  // )
+
+  const { nextSong, player } = usePlayerContext()
 
   const [isEnabled, setIsEnabled] = useState(true)
 
   // skip the current song, if list is finished and repeat mode enabled starts again
-  const next = () => {
+  const handleNextControl = () => {
     // this code only set the state to enable o disabled for styles of next button
-    if (repeatAll) {
-      setIsEnabled(true)
-    } else if (
-      !repeatAll &&
-      (currentSong.index + 1 === songs.length ||
-        currentSong.index === songs.length)
-    ) {
-      setIsEnabled(false)
-    }
-
-    if (currentSong.index >= songs.length) {
-      if (repeatAll) {
-        dispatch(startAgain())
-      }
-      isPlaying && continuePlaying(html_audio)
-    } else {
-      dispatch(nextSong())
-      isPlaying && continuePlaying(html_audio)
-    }
+    // if (repeatAll) {
+    //   setIsEnabled(true)
+    // } else if (
+    //   !repeatAll &&
+    //   (currentSong.index + 1 === songs.length ||
+    //     currentSong.index === songs.length)
+    // ) {
+    //   setIsEnabled(false)
+    // }
+    // if (currentSong.index >= songs.length) {
+    //   if (repeatAll) {
+    //     dispatch(startAgain())
+    //   }
+    //   isPlaying && continuePlaying(html_audio)
+    // } else {
+    //   dispatch(nextSong())
+    //   isPlaying && continuePlaying(html_audio)
+    // }
+    nextSong()
+    player.isPlaying && continuePlaying(player.html_audio)
+    console.log('next')
   }
 
   // autoplay if is possible
-  if (isPlaying && html_audio?.ended && isEnabled) next()
+  // if (html_audio?.ended && isEnabled) next()
 
   return (
     <button
-      onClick={next}
+      onClick={handleNextControl}
       aria-label='next button'
       className={`${isEnabled ? 'btn' : 'btn btn-disabled'}`}
     >
--- src/Player/Controls/PlayPause/ControlPlayPause.jsx
+++ src/Player/Controls/PlayPause/ControlPlayPause.jsx
@@ -1,17 +1,17 @@
 // external
-import { useDispatch, useSelector } from 'react-redux'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import PauseIcon from '@mui/icons-material/Pause'
 
 // utils
 import { pauseTrack, playTrack } from 'utils/helpers'
+import { usePlayerContext } from 'Player/Context'
 
 export const ControlPlayPause = () => {
-  const dispatch = useDispatch()
-  const { isPlaying, html_audio } = useSelector(({ player }) => player)
+  const { player, turnPlay } = usePlayerContext()
+  const { isPlaying, html_audio } = player
 
   const playPause = () => {
-    dispatch({ type: 'TURN_PLAY_PAUSE' })
+    turnPlay()
     isPlaying ? pauseTrack(html_audio) : playTrack(html_audio)
   }
 
