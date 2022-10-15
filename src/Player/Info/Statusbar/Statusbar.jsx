764479dc4b6b1bd33703be727afb68b914d72e04
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-15 01:33:56 -0300

    ref barra de reproduccion funcional

--- src/Player/Info/Statusbar/Statusbar.jsx
+++ src/Player/Info/Statusbar/Statusbar.jsx
@@ -1,51 +1,9 @@
 import { usePlayerContext } from 'Player/Context'
 import { useEffect, useState } from 'react'
+import { getPercent } from 'utils/helpers'
 
 export const Statusbar = () => {
-  // const handlePercentChange = (e) => {
-  //   let inputValue = Number(e.target.value)
-
-  //   if (inputValue === 100) {
-  //     setTimeout(() => {
-  //       if (isPlaying) {
-  //         setCurrentTime(0)
-  //         dispatch(nextSong())
-  //         continuePlaying(html_audio)
-  //       } else if (!isPlaying) {
-  //         console.log('a la posima cancion cuando este en play')
-  //       }
-  //     }, 500)
-  //   }
-
-  //   setCurrentTime(inputValue)
-  //   dispatch(setPercentPlayedExplicit(currentTime))
-
-  //   html_audio.currentTime =
-  //     Math.floor((currentSong.duration_sec / 100) * currentTime) || 0
-  //   // console.log(html_audio)
-  //   // regla de 3 para setear el porcentage entre 0 y 100 de reproduccion segun la duracion del tema actual
-
-  //   // dispatch(setPercentPlayed(html_audio, currentSong))
-  //   // console.log(Number(currentTime))
-  //   // if (
-  //   //   // isPlaying &&
-  //   //   Number(currentTime) === 96 ||
-  //   //   currentSong.percent_played === 100
-  //   // ) {
-  //   //   if (isPlaying) {
-  //   //     console.log(currentSong)
-  //   //     dispatch(nextSong())
-  //   //     console.log(currentSong)
-  //   //     setCurrentTime(0)
-  //   //   }
-  //   //   // console.log('no seguir')
-  //   // } else {
-  //   //   continuePlaying(html_audio)
-  //   //   // console.log('else')
-  //   // }
-  // }
-
-  const { player } = usePlayerContext()
+  const { player, nextSong } = usePlayerContext()
   const { currentSong, html_audio, isPlaying } = player
 
   const [percentPlayed, setPercentPlayed] = useState(0)
@@ -54,19 +12,29 @@ export const Statusbar = () => {
     let interval
     if (isPlaying) {
       interval = setInterval(() => {
-        let percent_played =
+        let percent_played = Math.round(
           (html_audio.currentTime * 100) / currentSong.duration_sec
-        setPercentPlayed(Math.round(percent_played))
+        )
+        setPercentPlayed(percent_played)
       }, 1000)
     }
     return () => clearInterval(interval)
-  }, [html_audio.currentTime, percentPlayed, isPlaying])
+  }, [isPlaying])
+
+  const handleChange = (e) => {
+    let value = Number(e.target.value)
+    setPercentPlayed(value)
+    html_audio.currentTime = getPercent(value, currentSong.duration_sec)
+  }
+
+  if (html_audio.ended) nextSong()
 
   return (
     <input
       type='range'
+      className='range-input'
       value={percentPlayed}
-      readOnly
+      onChange={handleChange}
       alt='percent playing'
       aria-label='Transcurred time of the song'
     />
