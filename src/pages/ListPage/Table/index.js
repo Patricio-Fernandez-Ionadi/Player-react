158f6deb88ef23b63e998caae234b00002f3f574
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-16 01:35:10 -0300

    update barrels

---
+++ src/pages/ListPage/Table/index.js
@@ -0,0 +1,4 @@
+export * from './Table'
+export * from './TableFoot'
+export * from './TableHead'
+export * from './TableItems'
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,1 +1,2 @@
 export * from './_playerReducer'
+export * from './playlistReducer'
