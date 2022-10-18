b56cea38d0b16638e9f3afc87bcba40856c68cb6
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-17 22:35:02 -0300

    ref no maneja estilos

--- src/app/Core.jsx
+++ src/app/Core.jsx
@@ -1,10 +1,12 @@
-// router-dom
 import { BrowserRouter } from 'react-router-dom'
-// redux
 import { Provider } from 'react-redux'
+
 import { store } from 'store'
-import { MenuMobileProvider } from 'context'
+import { MenuMobileProvider } from 'app/context'
+
 import { App } from './App'
+
+import '../theme/index.css'
 
 export const Core = () => {
   return (
