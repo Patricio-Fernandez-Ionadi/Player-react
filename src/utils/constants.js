62b0ddf18cce3fe116ea8f6f26b60af27de31e8f
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-17 22:01:56 -0300

    ref borrado de exportaciones innecesarias

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,1 @@
 export const baseApiUri = 'https://protected-springs-12776.herokuapp.com/api'
-
-export const colors = {
-  primary_light: '#00FFF5',
-  primary: '#00ADB5',
-  primary_dark: '#0D7377',
-  dark: '#323232',
-  darken: '#323232',
-  black: '#100F0F',
-  white: '#EEEEEE',
-  succes: '#C4FB6D',
-  error: '#FF2E63',
-  blue: '#2F89FC',
-  grey: '#474744',
-  grey_light: '#aaadb0',
-  background: '#045757',
-  background_dark: '#044343',
-}
