67b08c9038f62639f3c3e0b3fa43d2bac0937c55
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-17 21:57:45 -0300

    ref AppRouter ahora solo maneja rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+// styles
+import '../theme/index.css'
+import { CreatePlaylist, Playlist } from 'pages'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* PRIVATE ROUTES */}
+      <Route path='/' element={<h1>Inicio</h1>} />
+      <Route path='/playlist' element={<Playlist />} />
+      <Route path='/lista' element={<CreatePlaylist />} />
+      {/* PUBLIC ROUTES */}
+      <Route path='/login' element={<h1>Login</h1>} />
+      <Route path='/register' element={<h1>Register</h1>} />
+    </Routes>
+  )
+}
