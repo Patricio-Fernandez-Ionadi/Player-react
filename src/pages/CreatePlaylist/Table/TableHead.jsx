7c4ea6d81e10994b463224ed7e5b0e72c259ec39
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-22 02:55:34 -0300

    ref hace uso de funcionalidad añadida toggle createPlaylist

--- src/pages/CreatePlaylist/Table/TableHead.jsx
+++ src/pages/CreatePlaylist/Table/TableHead.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+import { useCreatePlaylist } from '../Context'
 export const TableHead = () => {
+  const { toggleAllSongs, isFullList } = useCreatePlaylist()
+
+  const [toggle, setToggle] = useState(true)
+
+  const handleToggle = () => {
+    toggleAllSongs()
+    setToggle(!toggle)
+  }
+
   return (
     <div className='thead flex'>
       <div className='non-mobile head-item'>n°</div>
       <div className='head-item'>Nombre</div>
       <div className='non-mobile head-item'>Album</div>
       <div className='head-item'>Artista</div>
-      <div className='head-item'>+</div>
+      <div className='head-item' onClick={handleToggle}>
+        {toggle || !isFullList ? '+' : '-'}
+      </div>
     </div>
   )
 }
