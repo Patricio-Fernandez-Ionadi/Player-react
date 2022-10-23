50e2304637b6e32dd6364a3c7034158edaad71b4
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-23 00:29:46 -0300

    add boton temporal modo oscuro/claro

--- src/app/Header/Navbar/Navbar.jsx
+++ src/app/Header/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import { NavbarList } from './NavbarList'
 import { Logo } from './Logo'
 import { Menu } from './Menu'
+import { useThemeContext } from 'app/context/Theme'
 
 export const Navbar = () => {
+  const { theme, toggleTheme } = useThemeContext()
+
   return (
-    <nav className={`navigation-container`}>
+    <nav className={`navigation-container ${theme}`}>
       <Logo />
+      <button onClick={toggleTheme}>theme</button>
       <NavbarList />
       <Menu />
     </nav>
