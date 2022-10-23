96a4c542434caaba247e77b571d85cab8ea48a48
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-23 00:35:29 -0300

    ref estilo flex

--- src/app/Header/Navbar/NavbarList/NavbarListElement.jsx
+++ src/app/Header/Navbar/NavbarList/NavbarListElement.jsx
@@ -3,7 +3,7 @@ import { NavbarListElementLinkIcon } from './NavbarListElementLinkIcon'
 
 export const NavbarListElement = (props) => {
   return (
-    <li className='navbar-list-element'>
+    <li className='navbar-list-element flex'>
       <NavbarListElementLink {...props} />
       <NavbarListElementLinkIcon {...props} />
     </li>
