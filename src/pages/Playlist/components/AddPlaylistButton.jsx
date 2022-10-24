e1a2ff37a387119c7756e7a9ad264384416b01d8
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-24 01:47:31 -0300

    re usa Button component

--- src/pages/Playlist/components/AddPlaylistButton.jsx
+++ src/pages/Playlist/components/AddPlaylistButton.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 import AddIcon from '@mui/icons-material/Add'
+import { Button } from 'components'
 
 export const AddPlaylistButton = () => {
   return (
-    <button>
+    <Button>
       <Link to='/lista'>
         New Playlist <AddIcon />
       </Link>
-    </button>
+    </Button>
   )
 }
