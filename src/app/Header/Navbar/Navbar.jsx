213e8f1f32b5ee0c26e0b0bb7121d2c5d582f9b2
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-24 01:56:50 -0300

    ref hace uso de componente ToggleTheme

--- src/app/Header/Navbar/Navbar.jsx
+++ src/app/Header/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import { NavbarList } from './NavbarList'
-import { Logo } from './Logo'
-import { Menu } from './Menu'
 import { useThemeContext } from 'app/context/Theme'
 
+import { Logo } from './Logo'
+import { ToggleTheme } from 'components'
+import { NavbarList } from './NavbarList'
+import { Menu } from './Menu'
+
 export const Navbar = () => {
-  const { theme, toggleTheme } = useThemeContext()
+  const { theme } = useThemeContext()
 
   return (
     <nav className={`navigation-container ${theme}`}>
       <Logo />
-      <button onClick={toggleTheme}>theme</button>
+      <ToggleTheme />
       <NavbarList />
       <Menu />
     </nav>
