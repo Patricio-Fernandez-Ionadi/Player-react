71ccb5d5a7268463ffae6364b6e9ccb50f0b22d4
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-25 18:45:28 -0300

    ref cambio en mostrado de informacion

--- src/pages/Playlist/AllPlaylists/PlaylistCard/PlaylistCardHeader/PlaylistCardHeader.jsx
+++ src/pages/Playlist/AllPlaylists/PlaylistCard/PlaylistCardHeader/PlaylistCardHeader.jsx
@@ -1,3 +1,7 @@
 export const PlaylistCardHeader = ({ name }) => {
-  return <h4 className='playlist-card-header'>{name}</h4>
+  return (
+    <h4 className='playlist-card-header'>
+      <small>Playlist:</small> {name}
+    </h4>
+  )
 }
