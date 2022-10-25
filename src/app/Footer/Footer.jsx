43ad49a7e25653ae768a3a738cefdd8f2b5de760
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-25 18:42:58 -0300

    ref añadido de contenido y estilos en Footer

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -1,27 +1,88 @@
-import React from 'react'
+import { useThemeContext } from 'app/context'
+
 export const Footer = () => {
+  const { theme } = useThemeContext()
+
   return (
-    <footer className='footer-container-app'>
-      <ul style={{ listStyle: ' none', padding: '0 0 0 20px' }}>
-        <li>
-          <a
-            href='https://github.com/Patricio-Fernandez-Ionadi/Player-react'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Ver codigo del proyecto
-          </a>
-        </li>
-        <li>
-          <a
-            href='http://github.com/Patricio-Fernandez-Ionadi'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vistita mi perfil de GitHub
-          </a>
-        </li>
-      </ul>
+    <footer className={`footer-container-app ${theme} flex`}>
+      <div>
+        <h4>Desarrollo</h4>
+        <ul style={{ listStyle: ' none', padding: 0 }}>
+          <li>
+            <a
+              href='https://github.com/Patricio-Fernandez-Ionadi/Player-react'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Ver codigo del proyecto
+            </a>
+          </li>
+          <li>
+            <a
+              href='http://github.com/Patricio-Fernandez-Ionadi'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Vistita mi perfil de GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Autor</h4>
+        <ul style={{ listStyle: ' none', padding: 0 }}>
+          <li>
+            <a
+              href='https://github.com/Patricio-Fernandez-Ionadi/Player-react'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              href='http://github.com/Patricio-Fernandez-Ionadi'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Youtube
+            </a>
+          </li>
+          <li>
+            <a
+              href='http://github.com/Patricio-Fernandez-Ionadi'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Contacto</h4>
+        <ul style={{ listStyle: ' none', padding: 0 }}>
+          <li>
+            <a
+              href='https://github.com/Patricio-Fernandez-Ionadi/Player-react'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Whatsapp
+            </a>
+          </li>
+          <li>
+            <a
+              href='http://github.com/Patricio-Fernandez-Ionadi'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Correo
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
