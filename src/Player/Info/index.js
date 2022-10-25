3b469bfe3b4f7b2f4c355e802f2ddb7c53cbcd6d
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-25 18:21:46 -0300

    update barrel

--- src/Player/Info/index.js
+++ src/Player/Info/index.js
@@ -1,2 +1,3 @@
 export * from './SongInfo'
 export * from './Statusbar'
+export * from './Info'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export * from './CreatePlaylist'
 export * from './Playlist'
+export * from './Inicio'
