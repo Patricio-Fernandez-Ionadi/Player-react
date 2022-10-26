12c1622609154112fa7165a177d974a09d39d499
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-26 19:18:16 -0300

    fix el estado se reseteaba al renderizar, ahora consume store

--- src/Player/Controls/Shuffle/Shuffle.jsx
+++ src/Player/Controls/Shuffle/Shuffle.jsx
@@ -4,12 +4,12 @@ import ShuffleIcon from '@mui/icons-material/Shuffle'
 import { usePlayerContext } from 'Player/Context'
 
 export const Shuffle = () => {
-  const { shuffle } = usePlayerContext()
+  const { shuffle, player } = usePlayerContext()
 
-  const [shuffleActive, setShuffleActive] = useState(false)
+  const [shuffleActive, setShuffleActive] = useState(player.shuffle)
   const handleShuffleToggle = () => {
     shuffle()
-    setShuffleActive(!shuffleActive)
+    setShuffleActive(!player.shuffle)
   }
   return (
     <button
