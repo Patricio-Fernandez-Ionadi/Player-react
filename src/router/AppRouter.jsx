9cd13fbf641c52cf58fa443469f5e31583b545ab
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-26 19:17:36 -0300

    ref AppRouter hace uso de objeto routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
-import { CreatePlaylist, Inicio, Playlist } from 'pages'
+import { About, CreatePlaylist, Inicio, Playlist } from 'pages'
+import { routes } from 'utils'
 
 export const AppRouter = () => {
   return (
     <Routes>
       {/* PRIVATE ROUTES */}
-      <Route path='/' element={<Inicio />} />
-      <Route path='/playlist' element={<Playlist />} />
+      <Route path={routes.home.route} element={<Inicio />} />
+      <Route path={routes.playlist.route} element={<Playlist />} />
       <Route path='/lista' element={<CreatePlaylist />} />
       {/* PUBLIC ROUTES */}
+      <Route path={routes.about.route} element={<About />} />
       <Route path='/login' element={<h1>Login</h1>} />
       <Route path='/register' element={<h1>Register</h1>} />
     </Routes>
