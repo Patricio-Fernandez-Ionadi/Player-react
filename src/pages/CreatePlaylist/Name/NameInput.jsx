f1cb404721adb08ada2b2acba19d025518c1f2c7
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-27 20:50:35 -0300

    ref hace uso de Tooltip para error

--- src/pages/CreatePlaylist/Name/NameInput.jsx
+++ src/pages/CreatePlaylist/Name/NameInput.jsx
@@ -1,24 +1,33 @@
 import { useState } from 'react'
 
 import { useCreatePlaylist } from '../Context'
+import { Tooltip } from 'components'
 
 export const NameInput = () => {
   const [playlistName, setPlaylistName] = useState('')
 
-  const { setName } = useCreatePlaylist()
+  const { setName, isExistentPlaylist } = useCreatePlaylist()
 
   const handleNameChange = (e) => {
     const value = e.target.value
     setPlaylistName(value)
     setName(value)
   }
+
   return (
-    <input
-      className='createplaylist-name-input'
-      type='text'
-      onChange={handleNameChange}
-      value={playlistName}
-      placeholder='Nombre de la playlist'
-    />
+    <Tooltip
+      condition={isExistentPlaylist}
+      title={'Una lista existente tiene este nombre. Por favor elige otro.'}
+    >
+      <input
+        className={`createplaylist-name-input ${
+          isExistentPlaylist ? 'error' : ''
+        }`}
+        type='text'
+        onChange={handleNameChange}
+        value={playlistName}
+        placeholder='Nombre de la playlist'
+      />
+    </Tooltip>
   )
 }
