6e6e61b94fcdc985ffdd0b091e51920f9b9cf133
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-30 12:33:29 -0300

    ref navegacion a detalle de playlist

--- src/pages/Playlist/AllPlaylists/PlaylistCard/PlaylistCardDetail/PlaylistCardDetail.jsx
+++ src/pages/Playlist/AllPlaylists/PlaylistCard/PlaylistCardDetail/PlaylistCardDetail.jsx
@@ -1,10 +1,25 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+
 export const PlaylistCardDetail = ({ playlist }) => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const handleMoreItems = (e) => {
+    navigate(`./detail/${playlist.name}`, { from: pathname })
+  }
+
   return (
     <ul className='playlist-card-detail'>
       <li className='playlist-card-detail-item'>{playlist.songs[0]?.name}</li>
       <li className='playlist-card-detail-item'>{playlist.songs[1]?.name}</li>
       <li className='playlist-card-detail-item'>{playlist.songs[2]?.name}</li>
-      <li className='playlist-card-detail-item'>...</li>
+      {playlist.songs?.length > 3 ? (
+        <li className='playlist-card-detail-item' onClick={handleMoreItems}>
+          y {playlist.songs?.length - 3} más...
+        </li>
+      ) : (
+        <span></span>
+      )}
     </ul>
   )
 }
