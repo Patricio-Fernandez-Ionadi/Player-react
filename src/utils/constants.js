70eb7f96ed724478954f89cebe5cdf860c78c965
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-10-30 15:29:37 -0300

    add footerList para links del footer y componentes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ import QueueMusicIcon from '@mui/icons-material/QueueMusic'
 import InfoIcon from '@mui/icons-material/Info'
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
 
+// Routes for App
 export const routes = {
   home: {
     route: '/',
@@ -33,6 +34,40 @@ export const routes = {
     menuItem: false,
     principal: false,
   },
+}
+
+// Footer info
+export const footerLists = {
+  dev: [
+    {
+      label: 'Ver codigo del proyecto',
+      url: 'https://github.com/Patricio-Fernandez-Ionadi/Player-react',
+    },
+    {
+      label: 'Vistita mi perfil de GitHub',
+      url: 'http://github.com/Patricio-Fernandez-Ionadi',
+    },
+  ],
+  social: [
+    {
+      label: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/patricio-fernandez-ionadi-12320015b/',
+    },
+    {
+      label: 'YouTube',
+      url: 'https://www.youtube.com/channel/UCbPO8LoaXFd5KKWf9ebGYiQ',
+    },
+    {
+      label: 'Instagram',
+      url: 'https://www.instagram.com/ionadipatriciofernandez/',
+    },
+  ],
+  contact: [
+    {
+      label: 'Correo',
+      url: 'mailto:patricio.f.i@live.com',
+    },
+  ],
 }
 
 // URI backend
