12a16fc3ed4a9256d4f2acd08d806a22bf3284b0
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-01 17:57:33 -0300

    ref renombramiento de tags y uso de componente Paper

--- src/pages/CreatePlaylist/CreatePlaylistC.jsx
+++ src/pages/CreatePlaylist/CreatePlaylistC.jsx
@@ -1,25 +1,24 @@
 import { useThemeContext } from 'app/context'
-import { Loading } from 'components'
+import { Loading, Paper } from 'components'
 import { useCreatePlaylist } from './Context'
 import { NameInput } from './Name'
 import { Table } from './Table'
 
 export const CreatePlaylistC = () => {
   const { songs } = useCreatePlaylist()
-
   const { theme } = useThemeContext()
 
   return (
-    <article>
+    <section className={`view ${theme}`}>
       <h2>Crea tu playlist</h2>
       {songs ? (
-        <div className={`paper ${theme}`}>
+        <Paper elevation={6}>
           <NameInput />
           <Table />
-        </div>
+        </Paper>
       ) : (
         <Loading />
       )}
-    </article>
+    </section>
   )
 }
