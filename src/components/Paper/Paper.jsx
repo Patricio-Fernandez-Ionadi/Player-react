3c153090cb19e4c7d85ed7b350346cc6b943048e
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-05 22:54:58 -0300

    ref ahora puede recibir events de click

--- src/components/Paper/Paper.jsx
+++ src/components/Paper/Paper.jsx
@@ -1,9 +1,13 @@
 import { useThemeContext } from 'app/context'
 import React from 'react'
-export const Paper = ({ children, elevation }) => {
+export const Paper = ({ children, elevation, onclick }) => {
   const { theme } = useThemeContext()
 
   const elevate = elevation ? `elev-${elevation}` : ''
 
-  return <article className={`paper ${theme} ${elevate}`}>{children}</article>
+  return (
+    <article className={`paper ${theme} ${elevate}`} onClick={onclick}>
+      {children}
+    </article>
+  )
 }
