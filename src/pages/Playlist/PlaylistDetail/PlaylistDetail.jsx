32a5829f10a3833b757e34353ce55a0c6cc414f0
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-05 22:58:39 -0300

    ref boton para seleccionar lista

--- src/pages/Playlist/PlaylistDetail/PlaylistDetail.jsx
+++ src/pages/Playlist/PlaylistDetail/PlaylistDetail.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { PrevPageButton } from 'pages/components'
+import { ArticleHeading, PrevPageButton } from 'pages/components'
 import { useThemeContext } from 'app/context'
+import { Button, Paper } from 'components'
+import { useDispatch } from 'react-redux'
+import { initTrackList } from 'store'
 
 export const PlaylistDetail = () => {
   const { playlistname } = useParams()
+  const dispatch = useDispatch()
   const { theme } = useThemeContext()
-
   const { playlists } = useSelector(({ playlist }) => playlist)
-
+  const { playlist: plpl } = useSelector(({ player }) => player)
   const selected = playlists.filter((e) => e.name === playlistname)[0]
 
   return (
     <section className={`view ${theme}`}>
-      <PrevPageButton />
-      <h2>{playlistname}</h2>
-      {selected?.songs?.map((e) => {
-        return <p key={e.name}>{e.name}</p>
-      })}
+      <nav className='flex'>
+        <PrevPageButton />
+        {selected?.name !== plpl?.name ? (
+          <Button primary onclick={() => dispatch(initTrackList(selected))}>
+            Seleccionar lista
+          </Button>
+        ) : (
+          <span></span>
+        )}
+      </nav>
+      <Paper>
+        <ArticleHeading title={playlistname} />
+        {selected?.songs?.map((e) => {
+          return <p key={e.name}>{e.name}</p>
+        })}
+      </Paper>
     </section>
   )
 }
