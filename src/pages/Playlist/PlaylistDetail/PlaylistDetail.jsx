2cee308a164a50bcbf324150a0589a0ce768961c
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-07 01:11:20 -0300

    ref comentarios para mejor entendiemiento de variables similares

--- src/pages/Playlist/PlaylistDetail/PlaylistDetail.jsx
+++ src/pages/Playlist/PlaylistDetail/PlaylistDetail.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { ArticleHeading, PrevPageButton } from 'pages/components'
+import { useSelector, useDispatch } from 'react-redux'
+
 import { useThemeContext } from 'app/context'
-import { Button, Paper } from 'components'
-import { useDispatch } from 'react-redux'
 import { initTrackList } from 'store'
 
+import { ArticleHeading, PrevPageButton } from 'pages/components'
+import { Button, Paper } from 'components'
+
 export const PlaylistDetail = () => {
-  const { playlistname } = useParams()
   const dispatch = useDispatch()
   const { theme } = useThemeContext()
+  // get the current playlist detail from url
+  const { playlistname } = useParams()
+
+  // all playlists in store
   const { playlists } = useSelector(({ playlist }) => playlist)
-  const { playlist: plpl } = useSelector(({ player }) => player)
+  // obtain the playlist object
   const selected = playlists.filter((e) => e.name === playlistname)[0]
+
+  // current playlist in store
+  const { playlist } = useSelector(({ player }) => player)
 
   return (
     <section className={`view ${theme}`}>
       <nav className='flex'>
         <PrevPageButton />
-        {selected?.name !== plpl?.name ? (
+        {selected?.name !== playlist?.name ? (
           <Button primary onclick={() => dispatch(initTrackList(selected))}>
             Seleccionar lista
           </Button>
