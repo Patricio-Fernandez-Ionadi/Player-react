476682e3650e1b237e5234b51946797994fddc30
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-08 17:56:02 -0300

    add vista detalle listas /playlist/detail/:name

---
+++ src/pages/Playlist/Detail/PlaylistDetail.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { useThemeContext } from 'app/context'
+import { initTrackList } from 'store'
+
+import { PrevPageButton } from 'pages/components'
+import { Button } from 'components'
+import { SinglePL } from '../components'
+
+export const PlaylistDetail = () => {
+  const dispatch = useDispatch()
+  const { theme } = useThemeContext()
+  // get the current playlist detail from url
+  const { playlistname } = useParams()
+
+  // all playlists in store
+  const { playlists } = useSelector(({ playlist }) => playlist)
+  // obtain the playlist object
+  const selected = playlists.filter((e) => e.name === playlistname)[0]
+
+  // current playlist in store
+  const { playlist } = useSelector(({ player }) => player)
+
+  return (
+    <section className={`view ${theme}`}>
+      <nav className='flex'>
+        <PrevPageButton />
+        {selected?.name !== playlist?.name ? (
+          <Button primary onclick={() => dispatch(initTrackList(selected))}>
+            Seleccionar lista
+          </Button>
+        ) : (
+          <span></span>
+        )}
+      </nav>
+      <SinglePL
+        playlist={selected}
+        iscurrent={selected?.name === playlist?.name}
+      />
+    </section>
+  )
+}
