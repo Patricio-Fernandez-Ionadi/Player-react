7c8da4aa36766b0e50b4076f6a0bed5b5a7537b2
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-08 16:49:47 -0300

    ref Paper ahora puede recibir clases desde la prop styles

--- src/components/Paper/Paper.jsx
+++ src/components/Paper/Paper.jsx
@@ -1,12 +1,15 @@
 import { useThemeContext } from 'app/context'
 import React from 'react'
-export const Paper = ({ children, elevation, onclick }) => {
+export const Paper = ({ children, elevation, onclick, styles }) => {
   const { theme } = useThemeContext()
 
   const elevate = elevation ? `elev-${elevation}` : ''
 
   return (
-    <article className={`paper ${theme} ${elevate}`} onClick={onclick}>
+    <article
+      className={`paper ${theme} ${elevate} ${styles ? styles : ''}`}
+      onClick={onclick}
+    >
       {children}
     </article>
   )
