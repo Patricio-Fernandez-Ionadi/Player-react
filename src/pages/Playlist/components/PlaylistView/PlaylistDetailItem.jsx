d9544e98b5bf8405f9adce668e38a1392db376ee
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-09 01:04:38 -0300

    ref al seleccionar una cancion isPlaying = true

--- src/pages/Playlist/components/PlaylistView/PlaylistDetailItem.jsx
+++ src/pages/Playlist/components/PlaylistView/PlaylistDetailItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { turnPlaying } from 'store'
 import { turnLoading } from 'store'
 import { setCurrentSong } from 'store'
 import { initTrackList } from 'store'
@@ -7,9 +8,12 @@ import { initTrackList } from 'store'
 export const PlaylistDetailItem = ({ element, index, playlist, iscurrent }) => {
   const dispatch = useDispatch()
 
-  const { currentIndex } = useSelector(({ player }) => player)
+  const { currentIndex, isPlaying } = useSelector(({ player }) => player)
 
   const handleSelectPlIndex = () => {
+    if (!isPlaying) {
+      dispatch(turnPlaying(true))
+    }
     dispatch(turnLoading(true))
     dispatch(setCurrentSong(false))
     dispatch(initTrackList(playlist, index))
