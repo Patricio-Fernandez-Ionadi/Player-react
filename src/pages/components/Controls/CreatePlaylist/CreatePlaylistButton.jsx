42f4b7c67dbd73a5089d97785887d12976af96a1
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-11-09 20:50:19 -0300

    ref movimiento de archivos para orden de proyecto

--- src/pages/components/Controls/CreatePlaylist/CreatePlaylistButton.jsx
+++ src/pages/components/Controls/CreatePlaylist/CreatePlaylistButton.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+
 import AddIcon from '@mui/icons-material/Add'
-import { Button } from 'components'
+
 import { useThemeContext } from 'app/context'
 
-export const AddPlaylistButton = () => {
+import { Button } from 'components'
+
+export const CreatePlaylistButton = () => {
   const { theme } = useThemeContext()
 
   return (
